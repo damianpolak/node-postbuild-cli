1296834d0de3ac44b21801fe3b083c3bffc40ff2
damianpolak <damianpolak.it@gmail.com>
2024-03-25 15:11:50 +0100

    feat: add no tasks found

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import { Logger } from './logger';
 import { registeredTasks } from './tasks';
 
 (async () => {
-  Logger.justlog('Start script');
-  for await (const task of config.tasks) {
-    await registeredTasks.find((x) => x.name === task)?.task();
+  if (config.tasks.length === 0) {
+    Logger.justlog('No tasks detected...');
+  } else {
+    Logger.justlog('Start script');
+    for await (const task of config.tasks) {
+      await registeredTasks.find((x) => x.name === task)?.task();
+    }
+    Logger.justlog('Successfully finished');
   }
-  Logger.justlog('Successfully finished');
 })();
